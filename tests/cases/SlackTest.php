bd8e2552aa583a1125a3941a59d743b912ba8f21
toni.lopez <toni.lopez@shazam.com>
2014-09-30 10:52:51 +0000

    ITL-724: Adding dummy test. Removing unnecessary elements from composer.json.

---
+++ tests/cases/SlackTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author toni <toni.lopez@shazam.com>
+ * @package PhpSlack\Tests
+ */
+
+namespace PhpSclack\Tests;
+
+use PhpSlack\Slack;
+use PhpSlack\Utils\RestApiClient;
+use PhpSlack\Tests\TestCase;
+
+class SlackTest extends TestCase
+{
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+    }
+}
