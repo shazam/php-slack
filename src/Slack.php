fc4517e6777dc7e995f88cdb0fc25dd6afe19204
toni.lopez <toni.lopez@shazam.com>
2015-01-20 09:28:30 +0000

    Adding license, fixing unit tests.

--- src/Slack.php
+++ src/Slack.php
@@ -1,9 +1,21 @@
 <?php
 
 /**
- * @package PhpSlack
- * @author toni <toni.lopez@shazam.com
+ * Copyright 2014 Shazam Entertainment Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * @author Toni Lopez <toni.lopez@shazam.com>
  */
+
 namespace PhpSlack;
 
 use Exception;
@@ -175,6 +187,7 @@ class Slack
             $response = $this->client->get('users.list');
 
             $users = array();
+            $userIds = array();
             foreach ($response['members'] as $user) {
                 $users[$user['profile']['email']] = array(
                     'id' => $user['id'],
